6a29a8f595d9e706c39d4182f5d6ca72305cacc0
Landon Johnson <Landon@Landons-Macbook.local>
2018-07-05 15:31:11 -0600

    finished part 7

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://landon:landon123@ds049744.mlab.com:49744/mern-traversy'
+}
---
+++ routes/api/posts.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+// @route GET api/posts/test
+// @desc Tests posts route
+// @access public
+router.get('/test', (req, res) => {
+    res.json({message: 'Posts works'})
+})
+
+module.exports = router;
