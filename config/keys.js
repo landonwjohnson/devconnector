435adb2a83b4f694a9628c429a9cff4e99e36fa5
Landon Johnson <Landon@Landons-Macbook.local>
2018-07-07 13:22:31 -0600

    set up server validation for login and register

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://landon:landon123@ds049744.mlab.com:49744/mern-traversy'
+    mongoURI: 'mongodb://landon:landon123@ds049744.mlab.com:49744/mern-traversy',
+    secretOrKey: 'secret'
 }
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -8,7 +10,7 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
-//Body Parser Middleware
+// Body Parser Middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -21,11 +23,13 @@ mongoose
     .then(() => console.log('MongoDB Connected') )
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => {
-    res.send('hello')
-})
+// Passport middleware
+app.use(passport.initialize());
 
-//Use Routes 
+// Passport Config
+require('./config/passport')(passport);
+
+// Use Routes 
 app.use('/api/users', users)
 app.use('/api/profile', profile)
 app.use('/api/posts', posts)
