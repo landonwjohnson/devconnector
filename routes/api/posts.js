1064e73620ae1837fb6c5baae942b55b9c411591
Landon Johnson <landonwjohnson@gmail.com>
2018-07-23 09:30:04 -0600

    user can now post and delete comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -12,9 +12,6 @@ const Post = require('../../models/Post');
 const validatePostInput = require('../../validation/post')
 
 
-
-
-
 // @route GET api/posts
 // @desc gets post
 // @access public
@@ -110,6 +107,36 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false}), (req
 })
 
 
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc remove comment from post
+// @access private
+
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', {session: false}), (req, res) => {
+
+    Post.findById(req.params.id)
+        .then(post => {
+            // Check to see if comment exists
+            if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0){
+                return res.status(404).json({commentnotexists: 'Comment does not exist'});
+            }
+
+            // Get remove index
+            const removeIndex = post.comments
+                .map(item => item._id.toString())
+                .indexOf(req.params.comment_id)
+
+            //Splice comment out of array
+            post.comments.splice(removeIndex, 1);
+            post.save().then(post => res.json(post))
+
+        })
+        .catch(err => res.status(404).json({postnotfound: 'No post found'}));
+
+
+
+})
+
+
 // @route POST api/posts/comment/:id
 // @desc add comment to post
 // @access private
@@ -139,7 +166,6 @@ router.post('/comment/:id', passport.authenticate('jwt', {session: false}), (req
             post.save().then(post => res.json(post))
         })
         .catch(err => res.status(404).json({postnotfound: 'No post found'}));
-
 })
 
 
