0f6a6b16a63bbcdb6b86855e598976c9348bd94e
Landon Johnson <landonwjohnson@gmail.com>
2018-07-24 10:29:18 -0600

    set up thunk middleware axios and redux for register page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
-import axios from 'axios';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
-import { registerUser } from '../../actions/authActions'; 
+ 
+import { withRouter } from 'react-router-dom';
+import { registerUser } from '../../actions/authActions';
 
 
 class Register extends Component {
@@ -18,6 +19,13 @@ class Register extends Component {
         }
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps){
+        if(nextProps.errors){
+            let { errors } = nextProps;
+            this.setState({errors})
+        }
     }
 
     onChange(e){
@@ -36,24 +44,20 @@ class Register extends Component {
             password2
         }
 
-        this.props.registerUser(newUser)
-
-        // axios.post('/api/users/register', newUser)
-        //     .then(res => console.log(res.data))
-        //     .catch(err => this.setState({errors: err.response.data}))
+        // this.props.registerUser(newUser, this.props.history)
+        this.props.registerUser(newUser, this.props.history)
     }
 
     render(){
         console.log(this.state.errors);
         const { errors } = this.state;
-        const { user } = this.props.auth;
         return(
         <div className="register">
             <div className="container">
             <div className="row">
                 <div className="col-md-8 m-auto">
                 <h1 className="display-4 text-center">Sign Up</h1>
-                <p className="lead text-center">Create your DevConnector account</p>
+                <p className="lead text-center">Create your Chads account</p>
                 <form noValidate onSubmit={this.onSubmit} action="create-profile.html">
                     <div className="form-group">
                     <input 
@@ -126,11 +130,13 @@ class Register extends Component {
 
 Register.propTypes = {
     registerUser: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
-    auth: state.auth
+    auth: state.auth,
+    errors: state.errors
 })
 
-export default connect(mapStateToProps, registerUser)( Register);
+export default connect(mapStateToProps, {registerUser})(withRouter(Register));
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
     return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
-        <Link to="/profiles" className="navbar-brand">DevConnector</Link>
+        <Link to="/profiles" className="navbar-brand">Chads Place ;)</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
             <span className="navbar-toggler-icon"></span>
         </button>
